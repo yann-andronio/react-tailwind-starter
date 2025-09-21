8376a5fbeaf885ff5407bb3eddf3006a6f58a731
Yann Andronio <todiveloyannandronio@gmail.com>
2025-09-21 20:33:19 +0300

    first commit

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// ni creeegna t@ tagnana
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-6xl font-bold text-red-800">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
